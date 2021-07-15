593d2f5b28dd9e98389b989a47490d5442ac8c23
nassir <daniya_n@yahoo.com>
2021-07-14 23:58:28 -0700

    column

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
@@ -17,7 +18,7 @@ export class TableComponent implements OnInit {
   }
 
   headers = ['store', '6:00AM', '7:00AM', '8:00AM', '9:00AM', '10:00AM', '11:00AM', '12:00PM', '1:00PM', '2:00PM', '3:00PM', '4:00PM', '5:00PM', '6:00PM', '7:00PM'];
-
+ columns = ['Seattle', 'Tokyo', 'Dubai', 'Paris', 'Lima']
   rows = [
     {'store ' : 'Seattle'}
   ]
