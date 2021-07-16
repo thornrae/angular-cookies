d208496932622723675ed2486a8d981615b1db3d
Taylor Thornton <thornrae@gmail.com>
2021-07-15 19:13:54 -0700

    garbage more or less

---
+++ src/app/assets/customersPerHour.js
@@ -0,0 +1,6 @@
+'use strict';
+
+
+let customersPerHour = (max, min) => {
+    return Math.floor(Math.random() * (max - min +1) + min)
+  }
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,29 +1,46 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { FormsModule } from '@angular/forms';
 import { Form } from '../form';
 
+// declare function customersPerHour(): void;
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
 
-export class FormComponent {
+export class FormComponent implements OnInit {
 
+  // myScriptElement: HTMLScriptElement;
+
+  // constructor() {
+  //   customersPerHour()
+  //   this.myScriptElement = document.createElement("script");
+  //   this.myScriptElement.src = "src/app/assets/customersPerHour.js"
+  //   document.body.appendChild(this.myScriptElement)
+  // }
+
+  ngOnInit() {
+    this.getStores();
+  }
   model = new Form("New City", 0, 0, 0);
 
   cityList: any[] = [ ];
 
-  hours: any[] = ['0700', '0800', '0900', '1000', '1100',
+  hours: any[] = ['store location', '0700', '0800', '0900', '1000', '1100',
                   '1200', '1300', '1400', '1500', '1600', 'Total']
 
   randomArr: any[] = [Math.random(), Math.random(), Math.random(), Math.random(), Math.random(),
                       Math.random(), Math.random(), Math.random(), Math.random(), Math.random()]
-
+  
   submitted = false;
 
   onSubmit(): void  {
     this.submitted = true;
+  }
+
+  getStores(): void {
+
   }
 
   newCity(value: { location: string;
@@ -37,7 +54,21 @@ export class FormComponent {
     console.log("value is", value)
     this.model = new Form(value.location, value.min, value.max, value.average)
     console.log("model", this.model)
+    console.log("city list", this.cityList)
     if (this.model) { this.cityList.push(value) }
   }
 
+  
+  
+  customersPerHour = (
+    min: number, 
+    max: number,
+  ) => {
+    return Math.floor(Math.random() * (max - min +1) + min)
+  }
+ 
+
+  // renderCustomersPerHour = () => {
+  //   for(var i=0; i < cityList)
+  // }
 }
